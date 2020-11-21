1a3bcd8adcb23775b38bbf6002ba9ba41d4820b7
Daniel Riccio <luissonic96@gmail.com>
2020-11-21 18:23:35 -0500

    Added header, home and about sections

--- website/src/components/PageWrapper.js
+++ website/src/components/PageWrapper.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class PageWrapper extends Component{
 
@@ -9,7 +10,7 @@ render(){
         <div>
               <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
                 <div className="container">
-                <a className="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+                <Link className="navbar-brand js-scroll-trigger" to="/">Lame Website</Link>
                 <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu
                     <i className="fas fa-bars"></i>
@@ -17,19 +18,22 @@ render(){
                 <div className="collapse navbar-collapse" id="navbarResponsive">
                     <ul className="navbar-nav text-uppercase ml-auto">
                     <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#services">Services</a>
+                        <Link className="nav-link js-scroll-trigger" to="/">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#portfolio">Portfolio</a>
+                        <Link className="nav-link js-scroll-trigger" to="/services">Services</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#about">About</a>
+                        <Link className="nav-link js-scroll-trigger" to="/portfolio">Portfolio</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#team">Team</a>
+                        <Link className="nav-link js-scroll-trigger" to="/about">About</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link js-scroll-trigger" href="#contact">Contact</a>
+                        <Link className="nav-link js-scroll-trigger" to="/team">Team</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link js-scroll-trigger" to="/contact">Contact</Link>
                     </li>
                     </ul>
                 </div>
