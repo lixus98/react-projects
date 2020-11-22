b1e6689dca4d57f1db1e45efdd5c9c23cba7f7ba
Daniel Riccio <luissonic96@gmail.com>
2020-11-22 14:15:54 -0500

    Added Portfolio to the page

--- website/src/components/Pages/Home.js
+++ website/src/components/Pages/Home.js
@@ -2,6 +2,12 @@ import React, {Component} from 'react';
 import Header from '../Common/Header';
 import image from '../assets/img/header-bg.jpg';
 
+//Re-usable components
+import Services from '../Common/Services';
+import Portfolio from '../Common/Portfolio';
+import Team from '../Common/Team';
+import Timeline from '../Common/Timeline';
+
 
 class Home extends Component{
 
@@ -18,6 +24,11 @@ render(){
                 showButton={true}
                 image={image}
             />
+            <Services />
+            <Portfolio />
+            <Timeline />
+            <Team />
+            
         </div>
 
     )
