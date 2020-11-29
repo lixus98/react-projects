480ccf654536bcdb4c70e5dbdfe08965d17f1539
Daniel Riccio <luissonic96@gmail.com>
2020-11-28 22:37:44 -0500

    Added new user

--- website-backend/server/server.js
+++ website-backend/server/server.js
@@ -36,6 +36,20 @@ boot(app, __dirname, function(err) {
 
 console.log(Object.keys(app.models));
 
+app.models.user.find((err, result) => {
+  if (result.length === 0) {
+    const user = {
+      email: 'lixus@email.com',
+      password: 'test',
+      username: 'lixus',
+    };
+
+    app.models.user.create(user, (err, result) => {
+      console.log("Tried to create user: ", err, result);
+    });
+  }
+});
+
 app.models.user.afterRemote('create', (ctx, user, next) => {
   console.log('New user is ', user);
   app.models.Profile.create({
