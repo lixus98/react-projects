03c525e80a5c6abd6fff6e617f8ec1233e4e4def
Daniel Riccio <luissonic96@gmail.com>
2020-11-30 20:43:01 -0500

    Working with data

--- website-backend/server/test.js
+++ website-backend/server/test.js
@@ -1,5 +1,25 @@
 var models = require('./server.js').models;
 
-models.Profile.findOrCreate({name:'Ana'}, (err, profile) => {
-    console.log("data?", err, profile)
-})
+// var names = ["Alberto", "Maria", "Pedro", "Sabrina", "Luis", "Jose", "Yuliana", "Will", "Johana", "Mari","Alberto", "Maria", "Pedro", "Sabrina", "Luis", "Jose", "Yuliana", "Will", "Johana", "Mari","Alberto", "Maria", "Pedro", "Sabrina", "Luis", "Jose", "Yuliana", "Will", "Johana", "Mari"];
+// var toSave = [];
+
+// for(var i = 0; i < 30; i++){
+//     toSave.push({name: `${names[i]}${i}`, email: `${names[i]}@gmail.com`});
+// }
+
+// toSave.map(obj => {
+//     models.Profile.create(obj, (updateError, updated) =>{
+//         console.log("Saved?: ",updateError, updated);
+//     });
+// });
+
+var filter = {
+    where:{
+        name:{like:'Sabrina'}
+    },
+    skip:1   
+}
+
+models.Profile.destroyById("5fc58c9c6c40c9014db40777", (err, found) => {
+    console.log("Found? ", err, found);
+});
