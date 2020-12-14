59b16edeebff6ee1a6041dd8b17b40e20cffe2e7
Daniel Riccio <luissonic96@gmail.com>
2020-12-13 22:41:57 -0500

    Add persisted store

--- website/src/index.js
+++ website/src/index.js
@@ -3,19 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
 import rootReducer from './store/reducers';
 import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react';
+import {createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(thunk)
-  );
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+let store = createStore(persistedReducer, applyMiddleware(thunk));
+let persistor = persistStore(store);
+
 
 ReactDOM.render(
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
     ,document.getElementById('root')
 );
